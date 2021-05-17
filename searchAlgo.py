e6e7e42ccac6ee70050409aaa74a7dcf4b18d5e0
Savio Cangussu <saviocangussu@hotmail.com>
2021-05-17 15:11:08 -0300

    Fixing texts

--- searchAlgo.py
+++ searchAlgo.py
@@ -2,6 +2,8 @@ from chessPieces.board import Board
 from chessPieces.knight import Knight
 import sys, math, time
 
+WARNING_SIZE = 11
+
 sys.setrecursionlimit(3000)
 
 def display(board, timer):
@@ -10,9 +12,9 @@ def display(board, timer):
     time.sleep(timer)
 
 def sortMoves(knight, coordinate):
-  return (-warnsdorffs(knight, coordinate))
+  return (-warnsdorff(knight, coordinate))
 
-def warnsdorffs(knight, coordinate):
+def warnsdorff(knight, coordinate):
   pos = [a + b for a,b in zip(knight.pos, coordinate)]
   return len(Knight.possiblePlacesForCoordinates(knight.board, *pos))
 
@@ -34,16 +36,16 @@ def startSearch(size, pos, timer = 0, color = 0):
   newKnight = Knight(newBoard, *pos)
 
   print("Iniciando algoritimo de busca")
-  print("Heurísticas usadas: Regra de Warnsdorffs")
+  print("Heurísticas usadas: Regra de Warnsdorff")
   print("Variávies:")
   print(f"- Tamanho do tabuleiro: {size} X {size}")
   print(f"- Posição inicial do cavalo: {pos[0]}:{pos[1]}")
   print(f"- Com tempo entre as jogas de {timer} segundos" if timer else "- Sem visualização")
   print()
 
-  if size >= 10:
+  if size >= WARNING_SIZE:
     print("AVISO")
-    print("Testes foram feitos com tabuleiros acima de tamanho 11 e a busca costuma demorar")
+    print(f"Testes foram feitos com tabuleiros acima de tamanho {WARNING_SIZE} e a busca costuma demorar")
   print("Iniciando em 3 segundos...:")
   time.sleep(3)
   print("Iniciado")
